f115ec2bb6be1104a861f305d8f3a50a159b43fd
Dpride5 <Dpride5@wgu.edu>
2025-02-04 19:50:34 +0000

    Finishing up tests for Unit Test

--- test_ml.py
+++ test_ml.py
@@ -40,16 +40,45 @@ def test_one():
 # TODO: implement the second test. Change the function name and input as needed
 def test_two():
     """
-    # add description for the second test
+    Test the machine learning model's algorithm.
     """
-    # Your code here
-    pass
+    data = pd.DataFrame(
+        {
+            "feature1": [1,2,3,4,5],
+            "feature2": [0,1,0,1,0],
+            "label": [0,1,0,1,0],
+        }
+    )
+    
+    X_train, y_train, _, _ = process_data(
+        data,
+        categorical_features = ["feature2"],
+        label="label",
+        training=True,
+    )
+
+    model = train_model(X_train, y_train)
+
+    assert isinstance(model, LogisticRegression)
 
 
 # TODO: implement the third test. Change the function name and input as needed
 def test_three():
     """
-    # add description for the third test
+    Testing if the compute_model_metrics function returns the expected values.
     """
-    # Your code here
-    pass
+    # Define a test set with controlled values
+    y_true = np.array([0,1,0,1,0])
+    y_pred = np.array([0,1,1,1,0])
+    
+    # Compute the metrics
+    precision, recall, fbeta = compute_model_metrics(y_true, y_pred)
+
+    # Checking if the computed metrics match the expected values
+    expected_precision = 0.6667
+    expected_recall = 1.0
+    expected_fbeta = 0.8
+
+    assert round(precision, 4) == expected_precision
+    assert round(recall, 4) == expected_recall
+    assert round(fbeta, 4) == expected_fbeta
