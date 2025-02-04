76b854526cc973da6ce4553c669ce8c4a71e1fbc
Dpride5 <Dpride5@wgu.edu>
2025-02-04 19:41:00 +0000

    Working on test_ml.py file

--- test_ml.py
+++ test_ml.py
@@ -1,14 +1,41 @@
 import pytest
-# TODO: add necessary import
+import numpy as pd
+from ml.data improt process_data
+from ml.model import (
+    compute_model_metrics,
+    train_model,
+)
+from sklearn.linear_model import LogisticRegression
 
 # TODO: implement the first test. Change the function name and input as needed
 def test_one():
     """
-    # add description for the first test
-    """
-    # Your code here
-    pass
+    Testing if the train_model function returns a model of the type that is expected.
 
+    """
+    data = pd.DataFrame(
+        {
+            "feature1": [1, 2, 3, 4, 5],
+            "feature2": [0, 1, 0, 1, 0],
+            "label": [0, 1, 0, 1, 0],
+        }
+    )
+
+    # Process data
+    X_train, y_train, _, _ = process_data(
+        data,
+        categorical_features=["feature2"],
+        label="label",
+        training=True,
+    )
+
+    #Train Model
+    model = train_model(X_train, y_train)
+
+    #Check if model returned train_model is of the expected type
+    expected_model_type = LogisticRegression
+
+    assert isinstance(model,expected_model_type)
 
 # TODO: implement the second test. Change the function name and input as needed
 def test_two():
